fa8cf81787ec8e79a839e175a8a2a15d48a82d0c
tengkufarhan00 <farhanalryan2@gmail.com>
2022-11-07 15:21:11 +0700

    initial commit

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,21 +1,9 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import RootHead from '@/components/Layouts/RootHead'
-import { storyblokInit, apiPlugin } from "@storyblok/react";
-import Article from "@/components/Storyblok/Article";
 import { ThemeProvider } from 'next-themes'
 
-const components = {
-  article: Article
-};
- 
-storyblokInit({
-  accessToken: process.env.NEXT_PUBLIC_STORYBLOK_API_TOKEN,
-  use: [apiPlugin],
-  components
-});
-
-export default function App({ Component, pageProps, session }: AppProps) {
+export default function App({ Component, pageProps }: AppProps) {
   return (
       <ThemeProvider attribute="class">
           <RootHead>
