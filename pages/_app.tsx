2f62f3826b8ee9015b18714fd13e5e86bd5d2326
tengkufarhan00 <farhanalryan2@gmail.com>
2022-11-07 15:53:56 +0700

    feature: add analytics

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,12 +2,14 @@ import '../styles/globals.css'
 import type { AppProps } from 'next/app'
 import RootHead from '@/components/Layouts/RootHead'
 import { ThemeProvider } from 'next-themes'
+import { Analytics } from '@vercel/analytics/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
       <ThemeProvider attribute="class">
           <RootHead>
             <Component {...pageProps} />
+            <Analytics />
           </RootHead>
       </ThemeProvider>
   )
