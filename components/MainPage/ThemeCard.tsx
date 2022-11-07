e907fea721be0b228c13ba2925eb805b99c33f3f
tengkufarhan00 <farhanalryan2@gmail.com>
2022-11-07 15:32:18 +0700

    minor change

--- components/MainPage/ThemeCard.tsx
+++ components/MainPage/ThemeCard.tsx
@@ -9,13 +9,15 @@ export default function ThemeCard() {
 				className='break-inside-avoid mb-4 grow h-24 bg-white dark:bg-dark rounded-3xl flex items-center justify-center dark:ring-2 dark:ring-neutral-800'>
 				<ToggleDark />
 			</motion.div>
-			<a href="https://ko-fi.com/tfarhan" target='_blank' rel="noreferrer">
-				<motion.div
-					whileTap={{ scale: 0.95 }} 
-					className='cursor-pointer break-inside-avoid mb-4 grow h-24 text-white font-semibold rounded-3xl flex items-center justify-center bg-gradient-to-br from-orange-300 to-rose-300 dark:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 transition-colors duration-500'>
-						<i class="bi bi-cup-hot-fill"></i>
-				</motion.div>
-			</a>
+			<motion.a 
+				href="https://ko-fi.com/tfarhan" 
+				target='_blank' 
+				rel="noreferrer"
+				whileTap={{ scale: 0.95 }} 
+				className='cursor-pointer break-inside-avoid mb-4 grow h-24 text-white font-semibold rounded-3xl flex items-center justify-center bg-gradient-to-br from-orange-300 to-rose-300 dark:bg-[radial-gradient(ellipse_at_bottom_right,_var(--tw-gradient-stops))] dark:from-amber-200 dark:via-violet-600 dark:to-sky-900 transition-colors duration-500'>
+			>
+				<i class="bi bi-cup-hot-fill"></i>
+			</motion.a>
 		</div>
 	)
 }
